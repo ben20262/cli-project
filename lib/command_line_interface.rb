d1c8bc9e409d80423049f0622cee21e4c8a4ff19
Benjamin Small <ben20262@aim.com>
2020-01-15 16:30:43 +0000

    cli upgrades

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,21 @@
-require 'nokogiri'
 require 'pry'
+
 class CommandLineInterface
-  def run
+
+  def self.run
+    puts "Please enter the url(s) that you want processed."
+    puts "If entering multiple please seperate them with a comma and a space."
+    input = gets.strip
+    array = input.split(", ")
+    array.each do |url|
+      if url.include?("wikipedia.org")
+        hash = Scraper.scrape_page(url)
+        name = hash.keys.first.to_s
+        name = Person.new(hash)
+      else
+        puts "#{url} was not a valid entry. Please remove or fix this entry."
+        run
+      end
+    end
   end
 end
