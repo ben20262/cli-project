cb9b18da2b1c4b2df34900590492ce4016acb0fb
Benjamin Small <ben20262@aim.com>
2020-01-17 23:12:58 +0000

    oo fli upgrades

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -12,13 +12,12 @@ class CommandLineInterface
         hash = Scraper.scrape_page(url)
         name = hash.keys.first.to_s
         name = Person.new(hash)
-        pp hash
       else
         puts "#{url} was not a valid entry. Please remove or fix this entry."
         run
       end
     end
-
+    info
   end
 
   def info
@@ -41,11 +40,12 @@ class CommandLineInterface
       person = Person.all[input]
       name = person.instance_variables[1].to_s.delete("@").split("_").join(" ").capitalize
     end
-
+    att_collection = []
     person.att_array.each do |att|
       count = 0
       att.instance_variables.each do |att_title|
-        if count > 0
+        att_array << att_title
+        if count > 0 && att_title != :@owner
           att_name = att_title.to_s.split("_").join(" ").delete("@").capitalize
           puts "To view information on #{name}'s #{att_name} press #{count}."
         end
@@ -61,16 +61,20 @@ class CommandLineInterface
     #   person.show_hash
     #   return
     else
-      key = person_hash[name].keys[input]
-      person_fact = person_hash[name][key]
+      choice = att_array[input]
     end
     count = 1
-    keys = person_fact.keys
-    keys.each do |key|
-      puts "To view information on #{name}'s #{key} press #{count}"
-      count += 1
+    fact_collection = []
+    choice.fact_array.each do |fact|
+      fact.instance_variables.each do |fact_title|
+        if fact_title != :@att_owner
+          fact_collection << fact_title
+          puts "To view information on #{name}'s #{fact_clean} press #{count}"
+          count += 1
+        end
+      end
     end
-    input = gets.strip.to_i
+    input = gets.strip.to_i - 1
     if input < 1 || input >= count
       puts "This is not a valid selection."
       info
