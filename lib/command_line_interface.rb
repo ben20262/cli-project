ae650e210f76d23d159b9459954ca0fc68338a70
Benjamin Small <ben20262@aim.com>
2020-01-17 17:24:36 +0000

    cli methods added

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CommandLineInterface
 
-  def self.run
+  def run
     puts "Please enter the url(s) that you want processed."
     puts "If entering multiple please seperate them with a comma and a space."
     input = gets.strip
@@ -17,6 +17,59 @@ class CommandLineInterface
         run
       end
     end
-    Person.all.first.show_hash
+    info
   end
+
+  def info
+    puts "What would you like to do?"
+    count = 1
+    people = Person.all
+    people.each do |person|
+      name = person.hash.keys[0].to_s
+      puts "To view information on #{name} enter #{count}."
+      count += 1
+    end
+    input = gets.strip.to_i - 1
+    if input < 0 || input >= count
+      puts "This is not a valid selection."
+      info
+    else
+      person = people[input]
+      person_hash = person.hash
+      name = person_hash.keys.first
+    end
+    count = 1
+    person_hash[name].each_key do |key|
+      puts "To view information on #{name}'s #{key} press #{count}."
+      count += 1
+    end
+    puts "To view all information on #{name} please enter #{count}."
+    input = gets.strip.to_i - 1
+    if input < 0 || input >= count
+      puts "This is not a valid selection."
+      info
+    elsif input == count - 1
+      person.show_hash
+      return
+    else
+      key = person_hash[name].keys[input]
+      person_fact = person_hash[name][key]
+    end
+    count = 1
+    binding.pry
+    keys = person_fact.keys
+    keys.each do |key|
+      puts "To view information on #{name}'s #{key} press #{count}"
+      count += 1
+    end
+    input = gets.strip.to_i - 1
+    if input < 0 || input >= count
+      puts "This is not a valid selection."
+      info
+    else
+      key = keys[input]
+      puts person_fact[key]
+    end
+  end
+
 end
