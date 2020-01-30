c334807d7451aac82eadb2c906cd1f2b0e3fc48a
Benjamin Small <ben20262@aim.com>
2020-01-30 22:25:55 +0000

    fixed crashing bug

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,7 +3,7 @@ class CommandLineInterface
   def run # directs the program
     puts "Please enter the url(s) that you want processed."
     puts "If entering multiple please seperate them with a comma and a space."
-    puts  "Enter exit to exit the application."
+    puts  "Enter exit at any time to exit the application."
     input = gets.strip
     array = input.split(", ")
     if input == ""
@@ -26,51 +26,57 @@ class CommandLineInterface
 
   def info # accesses the information of the Person instances either alone or in tandem
     puts "What would you like to do?"
-    puts "Enter 0 at any time to exit."
     if Person.all.size > 1 # skips if there is only one instance
-      count = 0
+      count = 1
       Person.all.each do |person| # for selecting which person or all persons you would like information on
-        count += 1
         name = person.name
         puts "To view information on #{name} enter #{count}."
+        count += 1
       end
-      puts "To compare all persons enter 3."
-      input = gets.strip.to_i - 1
-      if input < -1 || input > count
+      puts "To compare all persons enter #{count}."
+      input = gets.strip
+      index = input.to_i
+      if input == "exit"
+        return
+      elsif index < 1 || index > count
         puts "This is not a valid selection."
         info
-      elsif input == count
-        compare
-        return
-      elsif input == -1
-        return
+      elsif index == count
+        var = compare
+        if var == false
+          return
+        else
+          info
+        end
       else
-        person = Person.all[input]
+        person = Person.all[index - 1]
         name = person.name
       end
     else
       person = Person.all.first
       name = person.name
     end
-    count = 0
+    count = 1
+    return if person == nil
     person_att = person.att_array
     person_att.each do |att|
-      count += 1
       clean_name = att.att_name.to_s.split("_").join(" ").delete("@").capitalize # cleans variable names and utilizes them as information
       puts "To view information on #{name}'s #{clean_name} press #{count}."
+      count += 1
     end
     puts "To view all information on #{name} enter #{count}."
-    input = gets.strip.to_i - 1
-    if input < -1 || input >= count
+    input = gets.strip
+    index = input.to_i
+    if input == "exit"
+      return
+    elsif index < 1 || index > count
       puts "This is not a valid selection."
       info
-    elsif input == count - 1
+    elsif index == count
       person.show_all
-      return
-    elsif input == -1
-      return
+      info
     else
-      choice = person_att[input]
+      choice = person_att[index - 1]
     end
     count = 0
     choice.fact_array.each do |fact|
@@ -78,14 +84,15 @@ class CommandLineInterface
       fact_clean = fact.to_s.split("_").join(" ").delete("@").capitalize
       puts "To view information on #{name}'s #{fact_clean} enter #{count}."
     end
-    input = gets.strip.to_i - 1
-    if input < -1 || input >= count
+    input = gets.strip
+    index = input.to_i
+    if input == "exit"
+      return
+    elsif index < 1 || index > count
       puts "This is not a valid selection."
       info
-    elsif input == -1
-      return
     else
-      fact_key = choice.fact_array[input]
+      fact_key = choice.fact_array[index - 1]
       puts choice.instance_variable_get(fact_key)
     end
     info
@@ -96,13 +103,16 @@ class CommandLineInterface
     if mutual.size == 0
       puts "There are no mutual tags."
     else
-      mutual.each_index do |index|
-        potential_key = mutual[index].split("_").join(" ").delete("@").capitalize
-        puts "To compare information about #{potential_key} please enter #{index + 1}." # puts mutual tags and numbers them for easy input
+      mutual.each_index do |ind|
+        potential_key = mutual[ind].split("_").join(" ").delete("@").capitalize
+        puts "To compare information about #{potential_key} please enter #{ind + 1}." # puts mutual tags and numbers them for easy input
       end
-      input = gets.strip.to_i - 1
-      if mutual.size > input && input >= 0
-        mutual_key = mutual[input]
+      input = gets.strip
+      index = input.to_i
+      if input == "exit"
+        return false
+      elsif mutual.size >= index && index > 0
+        mutual_key = mutual[index - 1]
         key_name = mutual_key.split("_").join(" ").delete("@").capitalize
         Person.all.each do |person|
           m_value = ""
